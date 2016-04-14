66e3e683bfa48e3ed840195bb56ac10a25f56081
James Choi <jchoi2012@knights.ucf.edu>
2016-04-13 20:13:44 -0400

    process of adding user controller

--- server/models/user.js
+++ server/models/user.js
@@ -25,7 +25,8 @@ var userSchema = new mongoose.Schema({
   },
 
   resetPasswordToken: String,
-  resetPasswordExpires: Date
+  resetPasswordExpires: Date,
+  dateCreated: { type: 'Date', default: Date.now, required: true }
 });
 
 
