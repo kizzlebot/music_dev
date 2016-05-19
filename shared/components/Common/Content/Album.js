75c89c0156dd87f3d974d78afce840847f10a470
James Choi <jchoi2012@knights.ucf.edu>
2016-05-19 01:28:29 -0400

    update

--- shared/components/Common/Content/Album.js
+++ shared/components/Common/Content/Album.js
@@ -3,27 +3,27 @@ import moment from 'moment';
 import Track from './Track';
 
 export default class Album extends React.Component {
-  componentDidMount(){
-    if(this.props.id){
-
-    }
-    else if(this.props.album){
-      var {images, release_date, name} = this.props.album;
-      this.setState({...this.props});
-    }
-  }
-
   render(){
-    var {images, release_date, name} = this.props.album;
+    var {album = {images:['']}, tracks = {items:[]}} = this.props ;
+    var {images=[{url:null}]} = album;
+
+
+    // console.log(this.props);
+    if (!album || !tracks || !(tracks.items instanceof Array)) {
+      // console.log(album);
+      return (<div/>);
+    }
+
+
     return (
       <div className="album">
         <div className="album__info">
           <div className="album__info__art">
-            <img src={images && images.length > 0 && images[0].url} alt={name} />
+            <img src={images.length > 0 && images[0].url} alt={album.name} />
           </div>
           <div className="album__info__meta">
-            <div className="album__year">{moment(release_date).format('YYYY')}</div>
-            <div className="album__name">{name}</div>
+            <div className="album__year">{moment(this.props.album.release_date).format('YYYY')}</div>
+            <div className="album__name">{this.props.album.name}</div>
             <div className="album__actions">
               <button className="button-light save">Save</button>
               <button className="button-light more">
@@ -44,12 +44,27 @@ export default class Album extends React.Component {
                 <i className="ion-thumbsup" />
               </div>
             </div>
-            {/*{this.props.album.tracks.items.map(t => {
+            {tracks.items.map(t => {
                 var duration = new moment.duration(t.duration_ms);
                 var mins = (duration.minutes() < 10) ? `0${duration.minutes()}` : duration.minutes();
                 var secs = (duration.seconds() < 10) ? `0${duration.seconds()}` : duration.seconds();
-                return (<Track {...t} mins={mins} secs={secs}/>)
-            })}*/}
+                return (
+                  <div key={t.id} className="track">
+                    <div className="track__number">{t.track_number}</div>
+                    <div className="track__added">
+                      <i className="ion-checkmark-round added" />
+                    </div>
+                    <div className="track__title">{t.name}</div>
+                    <div className="track__explicit">
+                      <span className="label">{t.explicit ? 'Explicit' : ''}</span>
+                    </div>
+                    <div className="track__length">{`${mins}:${secs}`}</div>
+                    <div className="track__popularity">
+                      <i className="ion-arrow-graph-up-right" />
+                    </div>
+                  </div>
+                )
+            })}
           </div>
         </div>
       </div>
